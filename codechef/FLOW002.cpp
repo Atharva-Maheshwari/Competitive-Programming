0b20502d0a7b51edcf97045121b7c1c720bf8ddb
Atharva-Maheshwari <Atharva-Maheshwari@users.noreply.github.com>
2021-07-08 23:14:36 +0530

    Create FLOW002.cpp

---
+++ codechef/FLOW002.cpp
@@ -0,0 +1,17 @@
+//Problem-Find Remainder
+
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    int ans=a%b;
+	    cout<<ans<<"\n";
+	}
+	return 0;
+}
