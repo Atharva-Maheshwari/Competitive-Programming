0364c9f3c919f66e0be40e5e7a4438f60bc54e83
Atharva-Maheshwari <Atharva-Maheshwari@users.noreply.github.com>
2021-07-12 23:10:07 +0530

    Create FSQRT.cpp

---
+++ codechef/FSQRT.cpp
@@ -0,0 +1,16 @@
+//Problem-Finding Square Roots
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+       int n,sq;
+       cin>>n;
+       sq=sqrt(n);
+       cout<<sq<<endl;
+    }
+	return 0;
+}
