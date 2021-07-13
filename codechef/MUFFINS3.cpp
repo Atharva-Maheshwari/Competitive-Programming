f8a7351fa51938c0873388712390024b67ac431f
Atharva-Maheshwari <Atharva-Maheshwari@users.noreply.github.com>
2021-07-13 22:09:55 +0530

    Create MUFFINS3.cpp

---
+++ codechef/MUFFINS3.cpp
@@ -0,0 +1,16 @@
+//Problem-Packaging Cupcakes
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        cout<<(n/2)+1<<"\n";
+    }
+	return 0;
+}
