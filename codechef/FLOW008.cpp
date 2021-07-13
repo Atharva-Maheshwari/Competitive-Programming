d68d1cfe0d3d671f93a3755b5d3d17f2e301b2b1
Atharva-Maheshwari <Atharva-Maheshwari@users.noreply.github.com>
2021-07-13 22:11:48 +0530

    Create FLOW008.cpp

---
+++ codechef/FLOW008.cpp
@@ -0,0 +1,18 @@
+//Problem-Helping Chef
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n<10)
+        cout<<"Thanks for helping Chef!\n";
+        else
+        cout<<-1<<"\n";
+    }
+	return 0;
+}
