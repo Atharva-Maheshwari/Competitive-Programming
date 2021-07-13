982894de21db3409783dc7c112815dcef087bf11
Atharva-Maheshwari <Atharva-Maheshwari@users.noreply.github.com>
2021-07-13 22:15:24 +0530

    Create REMISS.cpp

---
+++ codechef/REMISS.cpp
@@ -0,0 +1,16 @@
+//Problem-Chef and Remissness
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        ll a,b;
+        cin>>a>>b;
+        cout<<max(a,b)<<" "<<a+b<<"\n";
+    }
+	return 0;
+}
