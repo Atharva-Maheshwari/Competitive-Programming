949e7b7ffd9888e159319412b9d8717b7c87e7eb
Atharva-Maheshwari <Atharva-Maheshwari@users.noreply.github.com>
2021-07-14 23:16:04 +0530

    Create DECINC.cpp

---
+++ codechef/DECINC.cpp
@@ -0,0 +1,14 @@
+//Problem-Decrement OR Increment
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    if(n%4==0)
+    cout<<++n;
+    else
+    cout<<--n;
+	return 0;
+}
