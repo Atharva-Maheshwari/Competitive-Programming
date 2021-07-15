c7ec735fb11e8a4b7f944f3fb4c677b005d4279a
Atharva-Maheshwari <Atharva-Maheshwari@users.noreply.github.com>
2021-07-15 22:22:32 +0530

    Create FLOW005.cpp

---
+++ codechef/FLOW005.cpp
@@ -0,0 +1,26 @@
+//Problem-Smallest Numbers of Notes
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+       int n,count=0;
+       cin>>n;
+       count=count+n/100;
+       n=n%100;
+       count=count+n/50;
+       n=n%50;
+       count=count+n/10;
+       n=n%10;
+       count=count+n/5;
+       n=n%5;
+       count=count+n/2;
+       n=n%2;
+       count=count+n;
+       cout<<count<<"\n";
+    }
+	return 0;
+}
