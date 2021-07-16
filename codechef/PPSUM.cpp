c4f41939b8944d3eb2b02d47a3498aafd89b6d7a
Atharva-Maheshwari <Atharva-Maheshwari@users.noreply.github.com>
2021-07-16 23:01:33 +0530

    Create PPSUM.cpp

---
+++ codechef/PPSUM.cpp
@@ -0,0 +1,19 @@
+//Problem-Puppy and Sum
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int d,n;
+        cin>>d>>n;
+        while(d>0){
+            n=(n*(n+1))/2;
+            d--;
+        }
+        cout<<n<<"\n";
+    }
+	return 0;
+}
