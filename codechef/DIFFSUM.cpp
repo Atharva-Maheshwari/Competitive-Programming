9c6e03ac4c59db631cf62ae9e5254aeab6c4fa2f
Atharva-Maheshwari <Atharva-Maheshwari@users.noreply.github.com>
2021-07-16 22:49:05 +0530

    Create DIFFSUM.cpp

---
+++ codechef/DIFFSUM.cpp
@@ -0,0 +1,14 @@
+//Problem-Sum OR Difference
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+    if(a>b)
+    cout<<a-b;
+    else
+    cout<<a+b;
+	return 0;
+}
