cbc15ff6afa8084080c350004fee7844482616a1
Atharva-Maheshwari <Atharva-Maheshwari@users.noreply.github.com>
2021-07-18 23:57:32 +0530

    Update FLOW009.cpp

--- codechef/FLOW009.cpp
+++ codechef/FLOW009.cpp
@@ -1,2 +1,23 @@
 //Problem-Total Expenses
 
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        ll qty,prc;
+        double expns;
+        cin>>qty>>prc;
+        
+        if(qty>1000)
+            expns=0.9*prc*qty;
+        else
+            expns=prc*qty;
+            
+        cout<<fixed<<setprecision(6)<<expns<<endl;
+    }
+	return 0;
+}
