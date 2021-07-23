6d370ce262d225d60acc2eb220cebc99a359d1e3
Atharva-Maheshwari <Atharva-Maheshwari@users.noreply.github.com>
2021-07-23 23:20:39 +0530

    Update FLOW014.cpp

--- codechef/FLOW014.cpp
+++ codechef/FLOW014.cpp
@@ -1,2 +1,28 @@
 //Problem-Grade The Steel
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int hrdn,tensl;
+        float cc;
+        cin>>hrdn>>cc>>tensl;
+        
+        if(hrdn>50 && cc<0.7 && tensl>5600)
+        cout<<10<<"\n";
+        else if(hrdn>50 && cc<0.7)
+        cout<<9<<"\n";
+        else if(tensl>5600 && cc<0.7)
+        cout<<8<<"\n";
+        else if(hrdn>50 && tensl>5600)
+        cout<<7<<"\n";
+        else if(hrdn>50 || cc<0.7 || tensl>5600)
+        cout<<6<<"\n";
+        else
+        cout<<5<<"\n";
+    }
+	return 0;
+}
