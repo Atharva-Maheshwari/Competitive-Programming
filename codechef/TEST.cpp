b2abde128a61064502ef5b14be519de6b040ec1b
Atharva-Maheshwari <Atharva-Maheshwari@users.noreply.github.com>
2021-07-24 23:53:58 +0530

    Update TEST.cpp

--- codechef/TEST.cpp
+++ codechef/TEST.cpp
@@ -1,2 +1,16 @@
 //Problem-Life, the Universe, and Everything
 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    while(!(n==42)){
+        cout << n <<"\n";
+        cin >> n;
+    }
+    
+	return 0;
+}
